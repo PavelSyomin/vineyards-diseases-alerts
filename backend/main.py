b5a211363cce24e7d646beeebf9c989ebf38bfd9
Pavel Syomin <pas.ntg@gmail.com>
2023-11-18 21:07:12 +0500

    Add CORS setup

--- backend/main.py
+++ backend/main.py
@@ -22,6 +22,23 @@ v = VineyardsManager(storage_path="vineyards.csv")
 
 app = FastAPI()
 
+# CORS setup
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "*",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/health")
 def health():
